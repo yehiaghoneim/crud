93ce1c128935663a189a03635da7e415afe6d9f4
yehiaghoneim <yehiamohamed01@yahoo.com>
2017-09-12 14:51:28 +0200

    crud
    crud

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using CRUD_With_Ajax_Bootstrap.Models;
+
+namespace CRUD_With_Ajax_Bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+     private readonly CompanyEntities _companyEntities=new CompanyEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            var allempaEmployees = _companyEntities.NewEmployees
+                .Join(_companyEntities.Departments, a => a.Department, q => q.DepartmentID, (a, q) => new
+                {
+                    a,q
+                }).Select(w=>new
+                {
+                    w.a.Employee_id,
+                    w.a.Name,
+                    w.a.Country,
+                    w.q.DepartmentName,
+                    w.a.age,
+                    w.a.state,
+                    w.a.Department
+                        
+                });
+            var finalllist = allempaEmployees.ToList();
+            return Json(finalllist, JsonRequestBehavior.AllowGet);
+            }
+        public JsonResult Add(NewEmployee emp)
+        {
+            _companyEntities.NewEmployees.Add(emp);
+           int x= _companyEntities.SaveChanges();
+            return Json(x, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Getbyid(int id)
+        {
+            NewEmployee employee = _companyEntities.NewEmployees.ToList().Find(q => q.Employee_id == id);
+            return Json(employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(NewEmployee emp)
+        {
+            var employee  = _companyEntities.Entry(emp).State = EntityState.Modified;
+            _companyEntities.SaveChanges();
+            return Json(employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int id)
+        {
+            
+          
+            return Json(_companyEntities.delete_emp(id), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Selectalldepartment()
+        {
+            List<Department> allDepartments = _companyEntities.Departments.ToList();
+
+            return Json(allDepartments, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Employee.js
@@ -0,0 +1,231 @@
+﻿//Load Data in Table when documents is ready  
+$(document).ready(function () {  
+    loadData();  
+});  
+  
+//Load Data function  
+function loadData() {  
+    $.ajax({  
+        url: "/Home/List",  
+        type: "GET",  
+        contentType: "application/json;charset=utf-8",  
+        dataType: "json",  
+        success: function (result) {  
+            var html = '';  
+            $.each(result, function (key, item) {  
+                html += '<tr>';  
+                html += '<td>' + item.Employee_id + '</td>';
+                html += '<td>' + item.Name + '</td>';  
+                html += '<td>' + item.age + '</td>';  
+                html += '<td>' + item.state + '</td>';  
+                html += '<td>' + item.Country + '</td>';
+                html += '<td>' + item.DepartmentName + '</td>';
+                html += '<td><a href="#" onclick="return getbyID(' + item.Employee_id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.Employee_id + ')">Delete</a></td>';
+                html += '</tr>';  
+            });
+            $('.tbody').html(html);  
+        },  
+        error: function (errormessage) {  
+            alert(errormessage.responseText);  
+        }  
+    });  
+}  
+  
+//Add Data Function   
+function Add() {  
+    var res = validate();  
+    if (res == false) {  
+        return false;  
+    }  
+    var empObj = {  
+        Employee_id: $('#Employee_id').val(),
+        Name: $('#Name').val(),  
+        Age: $('#Age').val(),  
+        state: $('#State').val(),  
+        country: $('#Country').val(),
+        Department: $('#countrySelect').val()
+    };  
+    $.ajax({  
+        url: "/Home/Add",  
+        data: JSON.stringify(empObj),  
+        type: "POST",  
+        contentType: "application/json;charset=utf-8",  
+        dataType: "json",  
+        success: function (result) {  
+            loadData();  
+            $('#myModal').modal('hide');
+        },  
+        error: function (errormessage) {  
+            alert(errormessage.responseText);  
+        }  
+    });  
+}
+
+
+$.ajax({
+        url: "/Home/Selectalldepartment",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json"
+
+    })
+    .success(function(data) {
+        DisplayCategories(data);
+
+    });
+
+
+
+
+function DisplayCategories(response) {
+    var getData = response;
+
+        var categoriesHtml = "";
+        for (var i = 0; i < getData.length; i++) {
+            $('#countrySelect').append($("<option ></option> ").val(getData[i].DepartmentID)
+                .html((getData[i].DepartmentName)));
+        };
+        
+    }
+
+
+
+
+
+
+//Function for getting the Data Based upon Employee ID  
+function getbyID(EmpID) {  
+    $('#Name').css('border-color', 'lightgrey');  
+    $('#Age').css('border-color', 'lightgrey');  
+    $('#State').css('border-color', 'lightgrey');  
+    $('#Country').css('border-color', 'lightgrey');  
+    $.ajax({  
+        url: "/Home/getbyID/" + EmpID,  
+        typr: "GET",  
+        contentType: "application/json;charset=UTF-8",  
+        dataType: "json",  
+        success: function (result) {  
+            $('#Employee_id').val(result.Employee_id);
+            $('#Name').val(result.Name);  
+            $('#Age').val(result.age);  
+            $('#State').val(result.state);  
+            $('#Country').val(result.Country);  
+            $('#countrySelect').val(result.Department);
+
+
+            $('#myModal').modal('show');  
+            $('#btnUpdate').show();  
+            $('#btnAdd').hide();  
+        },  
+        error: function (errormessage) {  
+            alert(errormessage.responseText);  
+        }  
+    });  
+    return false;  
+}  
+  
+//function for updating employee's record  
+function Update() {  
+    var res = validate();  
+    if (res == false) {  
+        return false;  
+    }  
+    var empObj = {  
+        Employee_id: $('#Employee_id').val(),
+        Name: $('#Name').val(),  
+        age: $('#Age').val(),  
+        state: $('#State').val(),  
+        Country: $('#Country').val(),
+        Department: $('#countrySelect').val()
+    };  
+    $.ajax({  
+        url: "/Home/Update",  
+        data: JSON.stringify(empObj),  
+        type: "POST",  
+        contentType: "application/json;charset=utf-8",  
+        dataType: "json",  
+        success: function (result) {  
+            loadData();  
+            $('#myModal').modal('hide');  
+            $('#Employee_id').val("");
+            $('#Name').val("");  
+            $('#Age').val("");  
+            $('#State').val("");  
+            $('#Country').val("");
+            $('#countrySelect').val("Select You Department");
+        },  
+        error: function (errormessage) {  
+            alert(errormessage.responseText);  
+        }  
+    });  
+}  
+  
+//function for deleting employee's record  
+function Delele(id) {
+    var ans = confirm("Are you sure you want to delete this Record?");  
+    if (ans) {  
+        $.ajax({  
+            url: "/Home/Delete/" + id,
+            type: "POST",  
+            contentType: "application/json;charset=UTF-8",  
+            dataType: "json",  
+            success: function (result) {  
+                loadData();  
+            },  
+            error: function (errormessage) {  
+                alert(errormessage.responseText);  
+            }  
+        });  
+    }  
+}  
+  
+//Function for clearing the textboxes  
+function clearTextBox() {  
+    $('#Employee_id').val("");
+    $('#Name').val("");  
+    $('#Age').val("");  
+    $('#State').val("");  
+    $('#Country').val("");
+    $('#countrySelect').val("Select Your Department"),
+    $('#btnUpdate').hide();  
+    $('#btnAdd').show();  
+    $('#Name').css('border-color', 'lightgrey');  
+    $('#age').css('border-color', 'lightgrey');  
+    $('#state').css('border-color', 'lightgrey');  
+    $('#country').css('border-color', 'lightgrey');  
+}  
+//Valdidation using jquery  
+function validate() {
+    var isValid = true;
+    if ($('#Name').val().trim() === "") {
+        $('#Name').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#Name').css('border-color', 'lightgrey');
+    }
+    if ($('#Age').val().trim()<=0) {
+        $('#Age').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#Age').css('border-color', 'lightgrey');
+    }
+    if ($('#State').val().trim() == "") {
+        $('#State').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#State').css('border-color', 'lightgrey');
+    }
+    if ($('#Country').val().trim() == "") {
+        $('#Country').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#Country').css('border-color', 'lightgrey');
+    }
+    if ($('#countrySelect').val().trim() == "Select Your Department") {
+        $('#countrySelect').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        $('#countrySelect').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
